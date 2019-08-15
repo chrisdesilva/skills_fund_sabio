fff2ed249c7c7722a7b571b5303fe3718dbce9cc
Chris DeSilva <chris.a.desilva@gmail.com>
2019-08-15 15:14:22 +0000

    Initial commit

---
+++ src/components/faq.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+// update with school-specific info
+const schoolInfo = {
+     schoolName: 'SCHOOL NAME',
+     programName: 'PROGRAM NAME',
+     minLoanAmt: '$2,000',
+     maxLoanAmt: 'MAX TUITION LOAN AMT',
+     costOfLiving: true,
+     costOfLivingAmt: 'COST OF LIVING AMT',
+     interestRate36: 'X.XX%',
+     APR36: 'XX.XX%',
+     IOPayment36: '$XX.XX',
+     interestRate60: 'XX.XX%',
+     APR60: 'XX.XX%',
+     IOPayment60: '$XX.XX'
+}
+
+const FAQ = () => (
+    <div className="border-2 border-gray-300 shadow-xl rounded-lg m-2 lg:m-10 p-8">
+       <h2 className="mt-4 text-center">Frequently Asked Questions</h2>
+       <h3 className="uppercase text-secondary">how much can i borrow and for what specific uses?</h3>
+            <p><strong>For the {schoolInfo.schoolName} program,</strong> you may borrow from {schoolInfo.minLoanAmt} to {schoolInfo.maxLoanAmt} for tuition. You may also borrow up to {schoolInfo.costOfLivingAmt} for cost of living.</p>
+            <p><strong>Please note:</strong> In order to finance cost of living, borrow at least {schoolInfo.minLoanAmt} in tuition financing. You will pay your cash deposit directly to <strong>{schoolInfo.schoolName}.</strong></p>
+     
+       {schoolInfo.costOfLiving &&
+       <div> 
+       <h3 className="uppercase text-secondary">when will i receive my living stipend?</h3>
+            <p>Your lump sum living stipend will be sent to you on the second Wednesday after your program start. You can elect to have your cost of living disbursed via electronic funds transfer or mailed directly to the address provided in their loan application.</p>
+            <p>Please allow 1 - 5 business days for your electronic funds transfer to be reflected in your bank account. For all students who elect to have funds mailed to their address, please allow 5 - 10 business days for your check to arrive via U.S. Standard Mail.</p>
+       </div>
+       }
+       <h3 className="uppercase text-secondary">how and when will i repay my loan?</h3>
+            <p>Upon loan acceptance, we will provide you with several options for making payments, including automated payments. You may create an account at <a href="https://www.aspireservicingcenter.com/" target="_blank" rel="noreferrer noopener" className="text-primary">AspireServicingCenter.com</a> at any time to manage payments and account balance.</p>
+            <p>You will start paying interest on your loan roughly one month after your loan is disbursed to the school. Disbursement occurs on the second Wednesday after program start. You will make interest-only payments while in program and for 60 days following program completion.</p>
+            <p>You will start making full monthly payments (interest plus principal) after your two-month grace period ends. This 60-day payment grace period helps you to keep your eye on finding the right job following program completion.</p>
+            <p>There is no prepayment penalty with a Skills Fund loan.</p>
+       
+       <h3 className="uppercase text-secondary">what is the deferment period?</h3>
+            <p>The deferment period is defined as the time you are attending the course, plus an additional two months after program completion. These additional two months are considered your grace period.</p>
+            <p>Interest-only payments are required during the deferment period. After the deferment period ends, payments of interest and principal are required. Paying interest on your loan during the deferment period will result in lower interest + principal payments during the full loan repayment phase of 36 or 60 months.</p>
+       
+       <h3 className="uppercase text-secondary">how much are interest payments during the deferment period?</h3>
+            <p>The interest-only payments depend on how much you borrow; the less you borrow, the less you will pay.</p>
+            <p><strong>For a 36-month $10,000 loan:</strong> The interest rate is fixed at {schoolInfo.interestRate36} / {schoolInfo.APR36} estimated APR. The interest-only monthly payment is approximately {schoolInfo.IOPayment36}.</p>
+            <p><strong>For a 60-month $10,000 loan:</strong> The interest rate is fixed at {schoolInfo.interestRate60} / {schoolInfo.APR60} estimated APR. The interest-only monthly payment is approximately {schoolInfo
+          .IOPayment60}.</p>
+            <p>Please see terms in "More Info On Terms" above.</p>
+            <p><strong>Please note:</strong> The Annual Percentage Rate (APR) is estimated and may change slightly based on the loan type, origination fee, and approximate program length. To learn how an Annual Percentage Rate (APR) is calculated, <a className="text-primary" target="_blank" href="https://skills.fund/resources/how-is-an-apr-calculated" rel="noreferrer noopener">visit our blog.</a></p>
+       
+       <h3 className="uppercase text-secondary">will i get charged any fees for taking out this loan?</h3>
+            <p>You will be charged an origination fee of 4.0%. This amount will be added to the amount of tuition you borrow and is included in the total loan principal amount you finance. This fee helps cover the administrative fees associated with originating the loan and is charged by our partner bank. Please see terms in "More Info on Terms" above.</p>
+            <p><strong>Please note:</strong> This fee is already reflected in the APR outlined above and in the loan calculator. Interested in learning more about your interest rate vs. APR? Check out our blog post <a className="text-primary" target="_blank" href="https://skills.fund/resources/apr-versus-interest-rates-whats-the-difference" rel="noreferrer noopener">here.</a></p>
+       
+       <h3 className="uppercase text-secondary">i am applying for a scholarship. should i wait to apply for financing?</h3>
+            <p>You may apply for financing in parallel to applying for your scholarship. If you are awarded your scholarship prior to the disbursement of your financing, please email <a href="mailto:CustomerTrust@Skills.Fund" className="text-primary">CustomerTrust@Skills.Fund</a> or notify your Admissions Counselor with the amount of your scholarship, and your requested financing amount will be downward adjusted.</p>
+            <p>Should you receive your scholarship following the second Wednesday after program start, you can apply your funds to your loan balance at any time without a prepayment penalty.</p>
+            <p><strong>Please note:</strong> Should you want to apply for a scholarship, it is recommended to apply for max tuition financing, and once your scholarship is awarded, Skills Fund can downward adjust your requested tuition financing. This may prevent you having to apply for additional funds, should you not be awarded your scholarship, as loans may not be upward adjusted.</p>
+       
+       <h3 className="uppercase text-secondary">do i need a cosigner?</h3>
+            <p>If you do not meet the loan underwriting criteria, a cosigner may be needed. You can (1) apply individually, and should you not be approved, you may be given the opportunity to amend your application with a cosigner, or (2) initiate your loan process with a cosigner.</p>
+            <p><strong>Please note:</strong> We advise you to apply individually to begin, and should you not be credit approved, you may be given the opportunity to amend your application with a cosigner.</p>
+       
+       <h3 className="uppercase text-secondary">when can i apply for a loan?</h3>
+            <p>Apply for the loan after your acceptance into a program. Your program cohort must begin within 90 days of the date that you apply for a loan.</p>
+       
+       <h3 className="uppercase text-secondary">what information do i need to provide skills fund?</h3>
+            <p>During the loan application process, we will ask you for the following information:</p>
+            <ul className="list-disc">
+                <li>Full name</li>
+                <li>Address</li>
+                <li>Email and other contact information</li>
+                <li>Social Security Number</li>
+                <li>Date of birth</li>
+                <li>Loan amount requested</li>
+                <li>U.S. Citizenship/Permanent Resident status</li>
+                <li>Current income <strong>(not a factor in credit decision)</strong></li>
+                <li>Current employment status <strong>(not a factor in credit decision)</strong></li>
+                <li>Three personal references</li>
+                <li>Cosigner information (if applicable)</li>
+            </ul>
+        
+        <h3 className="uppercase text-secondary">will you check my credit?</h3>
+            <p>Yes. When you apply we will check your credit, including your credit score. Interested in learning more about what goes into your credit score? Visit our blog and explore our <a className="text-primary" target="_blank" href="https://skills.fund/resources/financing-101" rel="noreferrer noopener">credit history resources</a> today.</p>
+        
+        <p className="text-left lg:text-center">For other general questions about Skills Fund financing, please visit our <a className="text-primary" target="_blank" href="https://skills.fund/resources/skills-fund-financing-faqs" rel="noreferrer noopener">Resource Center</a>.</p>
+    </div>
+)
+
+export default FAQ
